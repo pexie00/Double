cac34acd235dad22ce3d1722f3a8838905f57d07
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-01-24 11:53:37 +0500

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1605,14 +1605,14 @@ async def auto_filter(client, msg, spoll=False):
             search = search.lower()
             find = search.split(" ")
             search = ""
-            removes = ["mystery"]
+            removes = ["mystery" "upload", "horror", "thriller", "mystery", "print", "file", "send", "chahiye", "chiye", "movi", "bhejo", "dijiye", "jaldi", "bollywood", "hollywood", "karo"]
             for x in find:
                 if x in removes:
                     continue
                 else:
                     search = search + x + " "
-            search = re.sub(r"\b((one))", "", search, flags=re.IGNORECASE)
-            search = re.sub(r"\s", " ", search).strip()
+            search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new||bruh|broh|helo|that|find|dubbed|link|venum|iruka|pannunga|pannungga|anuppunga|anupunga|anuppungga|anupungga|film|undo|kitti|kitty|tharu|kittumo|kittum|movie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
+            search = re.sub(r"\s+", " ", search).strip()
             search = search.replace("-", " ")
             search = search.replace(":","")
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)

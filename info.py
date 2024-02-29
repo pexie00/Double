58b7aa0fff04814f8846b097f3d699364636db39
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-02-29 22:50:57 +0500

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #this shortlink working
 IMPORT_JK_SITE = environ.get('IMPORT_JK_SITE', 'nanolinks.in')
 IMPORT_JK_API = environ.get('IMPORT_JK_API', 'a71b1a34f1300901e5865e738267409d06059d43')
-IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), True)
+IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'True')), True)
 
 # Others
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)

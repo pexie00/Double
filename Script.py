24d79a053fed04d208b4c94445d9b9b723ddbf5c
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-06-12 11:50:13 +0500

    Update Script.py

--- Script.py
+++ Script.py
@@ -144,7 +144,7 @@ Nᴀᴍᴇ - {}"""
 ★ Message <code>{}</code>"""
     
     CAPTION = """
-<b>📂 Fɪʟᴇ ɴᴀᴍᴇ : </b><a href="https://t.me/hdlinks4uu">{file_name}</a>\n\nStream your file through this bot 👉: @FileStreamerv22_bot <b>
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ : </b><a href="https://t.me/hdlinks4uu">{file_name}</a>\n\nStream your file through this bot:👇<b>
 </b>"""
 
     IMDB_TEMPLATE_TXT = """

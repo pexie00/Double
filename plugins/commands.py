3c8382e23545ebef94c6d3989a90fba8ea68e2c3
pexie00 <155968186+pexie00@users.noreply.github.com>
2024-07-11 10:27:14 +0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,12 +43,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton('• ᴄʜᴀɴɴᴇʟ •', url=CHNL_LNK),
-                    InlineKeyboardButton('• ɢʀᴏᴜᴘ •', url=GRP_LNK)
-                ],[
-                    InlineKeyboardButton('• ᴅᴍᴄᴀ •', url='https://telegra.ph/DMCA-REPORT-11-28'),
+                    InlineKeyboardButton('• ᴅᴍᴄᴀ •', url='https://telegra.ph/DMCA-REPORT-11-28')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_text("ꜱᴛᴀʀᴛɪɴɢ") 

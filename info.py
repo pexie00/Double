0d3b9876539f9060bc74e347b4838e3a76f7b945
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-07-26 13:10:40 +0500

    Update info.py

--- info.py
+++ info.py
@@ -43,6 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
+SECONDDB_URI = environ.get('SECONDDB_URI', None)
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ORG:ORG@cluster0.xcsyn7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')

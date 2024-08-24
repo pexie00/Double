7ece2b91f8e52e01233ad4b077b014ed7a9b2a14
pexie00 <155968186+pexie00@users.noreply.github.com>
2024-08-24 08:01:00 +0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -268,7 +268,7 @@ async def start(client, message):
                     InlineKeyboardButton("⚠️ Hᴏᴡ Tᴏ Vᴇʀɪғʏ ⚠️", url=HOW_TO_VERIFY)
                 ]]
                 await message.reply_video(
-                    photo="https://graph.org/file/b78990ae332f917490448.mp4",
+                    video="https://graph.org/file/b78990ae332f917490448.mp4",
                     caption=f"<b>VERIFY TUTORIAL👆\n ʜᴇʟʟᴏ {message.from_user.mention}, ʏᴏᴜʀ ᴀʀᴇ ɴᴏᴛ ᴠᴇʀɪғɪᴇᴅ ᴛᴏᴅᴀʏ,ᴘʟᴇᴀꜱᴇ ᴠᴇʀɪғʏ ɴᴏᴡ ᴀɴᴅ ɢᴇᴛ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇꜱꜱ ғᴏʀ ɴᴇxᴛ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ for 30 hours 🚀\n\n click on the ♻️ Vᴇʀɪғʏ ♻️ butn to get files\nclicK on the ⚠️ Hᴏᴡ Tᴏ Vᴇʀɪғʏ ⚠️ if you face problem in verification\nVERIFY LINK 👇</b>",
                     reply_markup=InlineKeyboardMarkup(btn)
                 )
@@ -320,7 +320,7 @@ async def start(client, message):
             InlineKeyboardButton("⚠️ Hᴏᴡ Tᴏ Vᴇʀɪғʏ ⚠️", url=HOW_TO_VERIFY)
         ]]
         await message.reply_video(
-            photo="https://graph.org/file/b78990ae332f917490448.mp4",
+            video="https://graph.org/file/b78990ae332f917490448.mp4",
             caption=f"<b>VERIFY TUTORIAL👆\nʜᴇʟʟᴏ {message.from_user.mention},\nʏᴏᴜʀ ᴀʀᴇ ɴᴏᴛ ᴠᴇʀɪғɪᴇᴅ ᴛᴏᴅᴀʏ,ᴘʟᴇᴀꜱᴇ ᴠᴇʀɪғʏ ɴᴏᴡ ᴀɴᴅ ɢᴇᴛ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇꜱꜱ ғᴏʀ ɴᴇxᴛ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ for 30 hours 🚀\n\n click on the ♻️ Vᴇʀɪғʏ ♻️ butn to get verified\nclick on the ⚠️ Hᴏᴡ Tᴏ Vᴇʀɪғʏ ⚠️ if you face problem in verification\nVERIFY LINK 👇</b>",
             reply_markup=InlineKeyboardMarkup(btn)
         )

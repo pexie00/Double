646109a1599d764d1e17eb713c183ba77f236771
pexie00 <155968186+pexie00@users.noreply.github.com>
2024-10-17 07:39:36 +0500

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -7,6 +7,7 @@ import random
 import os
 lock = asyncio.Lock()
 import pyrogram
+from pyrogram.types import Message
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \

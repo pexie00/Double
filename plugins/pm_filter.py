63a2c7602226ad61f0acebf9dfa49b405771828d
pexie00 <155968186+pexie00@users.noreply.github.com>
2025-12-17 13:07:32 +0500

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -153,7 +153,7 @@ async def pm_text(bot, message : Message):
     if user_id in ADMINS: return # ignore admins
     link = await getInviteLinkIfInGroup(bot , user_id=user_id)
     msg = '<b>You are present in our group\nJoin To Get file 👇🏻</b>' if link else "<b>Please Join Our Group For Files 👇🏻</b>" 
-    link = link if link else 'https://t.me/+SCMPKFEq9oQwZWQ0'
+    link = link if link else 'https://telegra.ph/PM-MovieZ-12-06-2'
     btn = [[InlineKeyboardButton('Join' , url=link)]]
     reply_markup = InlineKeyboardMarkup(btn)
     await message.reply(text=msg , reply_markup=reply_markup)
